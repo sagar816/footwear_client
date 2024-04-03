62734a3a0fea68a60cb96468c32b0fcecea1d545
sagart <sagart@gmail.com>
2024-04-03 18:02:18 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/footwear_client/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.footwear_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:footwear_client/pages/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+//2.56.13
